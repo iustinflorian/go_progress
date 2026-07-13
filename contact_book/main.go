799b923c8d08d8a4cabe2252f3b29d7d935eb8a7
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-13 15:45:43 +0300

    feat: added setting contact as favourite and displaying favourite status in view contacts

--- contact_book/main.go
+++ contact_book/main.go
@@ -5,16 +5,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
 type contact struct {
 	prefix      string
 	phoneNumber string
+	isFavourite bool
 }
 
 func newContact(prefix string, phoneNumber string) *contact {
-	c := contact{prefix: prefix, phoneNumber: phoneNumber}
+	c := contact{prefix: prefix, phoneNumber: phoneNumber, isFavourite: false}
 	return &c
 }
 
@@ -45,7 +47,18 @@ func addContact(contactBook map[string]*contact, input *bufio.Reader) {
 	}
 
 	contactBook[name] = newContact(prefix, phoneNumber)
-	fmt.Print("\n")
+
+	fmt.Print("Do you want to mark it as favourite? (y/n): ")
+	choice, _ := input.ReadString('\n')
+	choice = strings.TrimSpace(choice)
+
+	if choice == "y" {
+		contactBook[name].isFavourite = true
+		fmt.Printf("Added contact %s as favourite\n\n", name)
+		return
+	}
+
+	fmt.Printf("Added contact %s\n\n", name)
 }
 
 func removeContact(contactBook map[string]*contact, input *bufio.Reader) {
@@ -64,20 +77,69 @@ func removeContact(contactBook map[string]*contact, input *bufio.Reader) {
 	fmt.Printf("Contact %s removed\n\n", name)
 }
 
-func showContact(contactBook map[string]*contact) {
-	fmt.Println("Contact Book:")
-	for name, info := range contactBook {
-		fmt.Printf(name + " -> " + info.prefix + info.phoneNumber)
+func updateContact(contactBook map[string]*contact, input *bufio.Reader) {
+	fmt.Print("Update contact:\nName: ")
+	name, _ := input.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Print("\n")
+
+	_, exists := contactBook[name]
+	if !exists {
+		fmt.Println("No contact found\n")
+		return
+	} else {
+		fmt.Printf("Contact %s exists. Enter new phone number: ", name)
 	}
-	fmt.Print("\n\n")
+
+	newNumber, _ := input.ReadString('\n')
+	newNumber = strings.TrimSpace(newNumber)
+
+	for _, info := range contactBook {
+		if info.phoneNumber == newNumber {
+			fmt.Printf("Contact with phone number %s already exists\n\n", newNumber)
+			return
+		}
+	}
+
+	contactBook[name].phoneNumber = newNumber
+	fmt.Printf("Phone number %s saved for contact %s\n\n", newNumber, name)
+}
+
+func showContact(contactBook map[string]*contact) {
+	if len(contactBook) == 0 {
+		fmt.Println("Empty contact book\n")
+		return
+	}
+
+	fmt.Println("Contact Book:")
+
+	names := make([]string, 0, len(contactBook))
+	for name := range contactBook {
+		names = append(names, name)
+	}
+
+	sort.Strings(names)
+
+	for _, name := range names {
+		info := contactBook[name]
+		fmt.Printf(name + " | " + info.prefix + info.phoneNumber)
+		if info.isFavourite {
+			fmt.Print(" | Favourite")
+		}
+		fmt.Print("\n")
+	}
+
+	fmt.Print("\n")
 }
 
 func showMenu(contactBook map[string]*contact, input *bufio.Reader) {
 	for {
 		fmt.Println("Menu")
-		fmt.Println("1. Add contact")
-		fmt.Println("2. Remove contact")
-		fmt.Println("3. View contacts")
+		fmt.Println("1. View contacts")
+		fmt.Println("2. Add contact")
+		fmt.Println("3. Update contact")
+		fmt.Println("4. Remove contact")
+
 		fmt.Print("Enter command (type 'no' to cancel): ")
 
 		command, _ := input.ReadString('\n')
@@ -86,11 +148,13 @@ func showMenu(contactBook map[string]*contact, input *bufio.Reader) {
 
 		switch command {
 		case "1":
-			addContact(contactBook, input)
-		case "2":
-			removeContact(contactBook, input)
-		case "3":
 			showContact(contactBook)
+		case "2":
+			addContact(contactBook, input)
+		case "3":
+			updateContact(contactBook, input)
+		case "4":
+			removeContact(contactBook, input)
 		case "no":
 			return
 		default:
@@ -102,6 +166,5 @@ func showMenu(contactBook map[string]*contact, input *bufio.Reader) {
 func main() {
 	input := bufio.NewReader(os.Stdin)
 	contactBook := make(map[string]*contact)
-
 	showMenu(contactBook, input)
 }
