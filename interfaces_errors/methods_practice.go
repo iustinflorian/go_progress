19c2d6a30bc1742e59ee7a0bd0358c73e0261a42
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-20 11:53:45 +0300

    feat: methods and pointers 2/2

--- interfaces_errors/methods_practice.go
+++ interfaces_errors/methods_practice.go
@@ -6,7 +6,11 @@ type Rectangle struct {
 	length, width float64
 }
 
-func Area(r Rectangle) float64 {
+func Area(r *Rectangle) float64 {
+	return r.length * r.width
+}
+
+func (r *Rectangle) Area() float64 {
 	return r.length * r.width
 }
 
@@ -15,9 +19,16 @@ func Scale(r *Rectangle, f float64) {
 	r.width *= f
 }
 
+func (r *Rectangle) Scale(f float64) {
+	r.length *= f
+	r.width *= f
+}
+
 func main() {
-	r := Rectangle{1, 2}
+	r := &Rectangle{1, 2}
 	fmt.Println(Area(r))
-	Scale(&r, 10)
-	fmt.Println(r)
+	Scale(r, 10)
+	r.Scale(10)
+	fmt.Println(*r)
+	fmt.Println(r.Area())
 }
