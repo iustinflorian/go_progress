9e7cc8441d6cd7ceff04dc9e04f9ba25fc1b884d
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-22 11:31:07 +0300

    feat: added showStudentInfo func; todo: use ID instead of passing object

--- assignments/school_grading_system/main.go
+++ assignments/school_grading_system/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// todo: functions that show info to use ID instead of passing the struct
 
 type Student struct {
 	name   string
 	group  string
 	grades []float64
+}
+
+func showStudentInfo(student Student) {
+	sum := 0.0
+
+	for _, grade := range student.grades {
+		sum += grade
+	}
+
+	sum /= float64(len(student.grades))
+	fmt.Printf("Student %s from group %s has an average grade of %f.\n", student.name, student.group, sum)
 }
 
 func generateAvgPerGroup(group string, school map[string]Student) float64 {
@@ -24,7 +40,6 @@ func generateAvgPerGroup(group string, school map[string]Student) float64 {
 	}
 
 	avg /= count
-
 	return avg
 }
 
@@ -32,20 +47,24 @@ func main() {
 	school := map[string]Student{
 		"01": Student{
 			"Andrei",
-			"4B",
+			"4b",
 			[]float64{9.3, 9.5, 8.4},
 		},
 		"02": Student{
 			"Matei",
-			"4B",
+			"4b",
 			[]float64{7.7, 6.3, 7.4},
 		},
 		"03": Student{
 			"Toni",
-			"4C",
+			"4c",
 			[]float64{7.4, 9.1, 6.0},
 		},
 	}
 
-	fmt.Printf("For group %s, average of grades is: %f\n", "4B", generateAvgPerGroup("4B", school))
+	showStudentInfo(school["01"])
+
+	group := "4B"
+	group = strings.ToLower(group)
+	fmt.Printf("For group %s, average of grades is: %f\n", group, generateAvgPerGroup("4b", school))
 }
