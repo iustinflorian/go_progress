cd5ba86d24de7488ae0a2a65d5c3a604169eeae6
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-24 10:53:10 +0300

    feat: added LOCATION env variable

--- labs/docker/main.go
+++ labs/docker/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var count int
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		location := os.Getenv("LOCATION")
+		if location == "" {
+			location = "Go"
+		}
+
 		count++
-		fmt.Fprintf(w, "<h1>Hello from Go! This page has been visited %d times</h1>", count)
+
+		fmt.Fprintf(w, "<h1>Hello from %s! This page has been visited %d times</h1>", location, count)
 	})
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		return
 	}
