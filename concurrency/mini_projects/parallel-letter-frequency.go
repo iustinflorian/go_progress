12d1e9a91ea1933133279bcbb826a96b34788356
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-27 16:55:33 +0300

    feat: complete rabbitmq lab assignment - add new goroutines exercise (from exercism) parallel letter frequency

--- concurrency/mini_projects/parallel-letter-frequency.go
+++ concurrency/mini_projects/parallel-letter-frequency.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+/*
+Count the frequency of letters in texts using parallel computation.
+Parallelism is about doing things in parallel that can also be done sequentially.
+A common example is counting the frequency of letters.
+Employ parallelism to calculate the total frequency of each letter in a list of texts.
+*/
+
+func Frequency(s string) map[rune]int {
+	freqMap := make(map[rune]int)
+	for _, letter := range s {
+		if unicode.IsLetter(letter) {
+			freqMap[letter]++
+		}
+	}
+	return freqMap
+}
+
+func main() {
+	text := []string{
+		"this is the first string",
+		"this is the second string",
+		"this is the third string",
+	}
+
+	var finalMap = make(map[rune]int)
+	mapChan := make(chan map[rune]int)
+
+	for _, str := range text {
+		go func(s string) {
+			mapChan <- Frequency(s)
+		}(str)
+	}
+
+	for i := 0; i < len(text); i++ {
+		partialMap := <-mapChan
+		for letter, count := range partialMap {
+			finalMap[letter] += count
+		}
+	}
+
+	fmt.Printf("Frequency of letter '%s' is: %d\n", "e", finalMap['e'])
+	fmt.Printf("Frequency of letter '%s' is: %d\n", "a", finalMap['a'])
+	fmt.Printf("Frequency of letter '%s' is: %d\n", "t", finalMap['t'])
+}
---
+++ labs/rabbitmq/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const url = "amqp://guest:guest@rabbitmq:5672/"
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("go run main.go [producer|consumer1|consumer2]")
+	}
+
+	var conn *amqp.Connection
+	var err error
+	for range 5 {
+		conn, err = amqp.Dial(url)
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		log.Fatal("can't connect to rabbitmq:", err)
+	}
+	defer conn.Close()
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	ch.ExchangeDeclare("dlx", "direct", true, false, false, false, nil)
+	ch.QueueDeclare("dlq", true, false, false, false, nil)
+	ch.QueueBind("dlq", "dlq_key", "dlx", false, nil)
+
+	// Coada simplă (setată să trimită în DLX dacă un mesaj eșuează)
+	args := amqp.Table{"x-dead-letter-exchange": "dlx", "x-dead-letter-routing-key": "dlq_key"}
+	ch.QueueDeclare("queue_start", true, false, false, false, args)
+
+	// pub/sub - 1 msg to 2 queues
+	ch.ExchangeDeclare("broadcast", "fanout", true, false, false, false, nil)
+	q1, _ := ch.QueueDeclare("queue_pubsub_1", true, false, false, false, nil)
+	q2, _ := ch.QueueDeclare("queue_pubsub_2", true, false, false, false, nil)
+	ch.QueueBind(q1.Name, "", "broadcast", false, nil)
+	ch.QueueBind(q2.Name, "", "broadcast", false, nil)
+
+	switch os.Args[1] {
+	case "producer":
+		ctx := context.Background()
+
+		// prod -> cons
+		ch.PublishWithContext(ctx, "", "queue_start", false, false, amqp.Publishing{Body: []byte("simple")})
+
+		// prod -> 2 cons
+		ch.PublishWithContext(ctx, "broadcast", "", false, false, amqp.Publishing{Body: []byte("broadcast")})
+
+		// prod -> wrong msg -> DLQ
+		ch.PublishWithContext(ctx, "", "queue_start", false, false, amqp.Publishing{Body: []byte("wrong")})
+
+		fmt.Println("[prod] sent all msg")
+
+	case "consumer1":
+		fmt.Println("[cons 1] start")
+		m1, _ := ch.Consume("queue_start", "", false, false, false, false, nil)
+		m2, _ := ch.Consume("queue_pubsub_1", "", true, false, false, false, nil)
+
+		go func() {
+			for d := range m1 {
+				if string(d.Body) == "wrong" {
+					fmt.Println("[cons 1] wrong msq, sent to dlq")
+					d.Nack(false, false) // nack + false = to dlq
+				} else {
+					fmt.Printf("[cons 1] got simple queue: %s\n", d.Body)
+					d.Ack(false)
+				}
+			}
+		}()
+
+		go func(){
+			for d := range m2 {
+				fmt.Printf("[cons 1] got broadcast: %s\n", d.Body)
+			}
+		}()
+
+		select {}
+
+	case "consumer2":
+		fmt.Println("[cons 2] start")
+		m1, _ := ch.Consume("queue_pubsub_2", "", true, false, false, false, nil)
+		m2, _ := ch.Consume("dlq", "", true, false, false, false, nil)
+
+		go func() {
+			for d := range m1 {
+				fmt.Printf("[cons 2] got broadcast: %s\n", d.Body)
+			}
+		}()
+
+		go func() {
+			for d := range m2 {
+				fmt.Printf("[cons 2 - dlq] got dlq msg: %s\n", d.Body)
+			}
+		}()
+
+		select {}
+	}
+}
