ea5ed50f070008d03305a7173a54c0d5ab4c2a1c
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-28 13:50:14 +0300

    feat: add simulation of user defined number of single matches with duration display

--- assignments/ping-pong.go
+++ assignments/ping-pong.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -20,7 +21,7 @@ type Player struct {
 	key  string
 }
 
-func player(player Player, teamChan chan Ball, oppositeChan chan Ball, stopChan chan Ball, wg *sync.WaitGroup) {
+func player(player Player, teamChan chan Ball, oppositeChan chan Ball, stopChan chan Ball, wg *sync.WaitGroup, show bool) {
 	defer wg.Done()
 
 	for {
@@ -43,15 +44,19 @@ func player(player Player, teamChan chan Ball, oppositeChan chan Ball, stopChan 
 				continue
 			}
 
-			if rand.Float32() < 0.02 {
-				fmt.Printf("[%s] didn't catch the ball (%d hits)\n", player.name, ball.hits)
+			if rand.Float32() < 0.4 {
+				if show {
+					fmt.Printf("[%s] didn't catch the ball (%d hits)\n", player.name, ball.hits)
+				}
 				close(stopChan)
 				return
 			}
 
 			ball.hits++
 			ball.lastPlayer = player
-			fmt.Printf("[%s] hit the ball (%d hits)\n", player.name, ball.hits)
+			if show {
+				fmt.Printf("[%s] hit the ball (%d hits)\n", player.name, ball.hits)
+			}
 
 			time.Sleep(1 * time.Second)
 
@@ -64,8 +69,10 @@ func player(player Player, teamChan chan Ball, oppositeChan chan Ball, stopChan 
 	}
 }
 
-func runSingleMatch() {
-	fmt.Println("[single] start match")
+func runSingleMatch(show bool) {
+	if show {
+		fmt.Println("[single] start match")
+	}
 
 	var wg sync.WaitGroup
 	pingChan := make(chan Ball)
@@ -73,14 +80,18 @@ func runSingleMatch() {
 	stopChan := make(chan Ball)
 
 	wg.Add(2)
-	go player(Player{"ping", ""}, pingChan, pongChan, stopChan, &wg)
-	go player(Player{"pong", ""}, pongChan, pingChan, stopChan, &wg)
+	go player(Player{"ping", ""}, pingChan, pongChan, stopChan, &wg, show)
+	go player(Player{"pong", ""}, pongChan, pingChan, stopChan, &wg, show)
 
-	fmt.Println("[single] first ball")
+	if show {
+		fmt.Println("[single] first ball")
+	}
 	pingChan <- Ball{0, Player{}}
 
 	wg.Wait()
-	fmt.Println("[single] match is over")
+	if show {
+		fmt.Println("[single] match is over")
+	}
 }
 
 func runDoubleMatch() {
@@ -92,10 +103,10 @@ func runDoubleMatch() {
 	stopChan := make(chan Ball)
 
 	wg.Add(4)
-	go player(Player{"ping", "A"}, teamAChan, teamBChan, stopChan, &wg)
-	go player(Player{"pong", "A"}, teamAChan, teamBChan, stopChan, &wg)
-	go player(Player{"peng", "B"}, teamBChan, teamAChan, stopChan, &wg)
-	go player(Player{"pang", "B"}, teamBChan, teamAChan, stopChan, &wg)
+	go player(Player{"ping", "A"}, teamAChan, teamBChan, stopChan, &wg, true)
+	go player(Player{"pong", "A"}, teamAChan, teamBChan, stopChan, &wg, true)
+	go player(Player{"peng", "B"}, teamBChan, teamAChan, stopChan, &wg, true)
+	go player(Player{"pang", "B"}, teamBChan, teamAChan, stopChan, &wg, true)
 
 	fmt.Println("[double] first ball")
 	teamAChan <- Ball{0, Player{}}
@@ -104,7 +115,35 @@ func runDoubleMatch() {
 	fmt.Println("[double] match is over")
 }
 
-//func runSimulation(input *bufio.Reader) { }
+func runSimulation(input *bufio.Reader) {
+	fmt.Print("[simulation] how many matches to simulate? ")
+
+	command, _ := input.ReadString('\n')
+	command = strings.TrimSpace(command)
+	count, err := strconv.Atoi(command)
+	if err != nil || count <= 0 {
+		fmt.Println("[simulation] invalid match count")
+		return
+	}
+
+	fmt.Printf("[simulation] starting %d matches\n", count)
+	startTime := time.Now()
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= count; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			runSingleMatch(false)
+		}()
+	}
+
+	wg.Wait()
+
+	duration := time.Since(startTime)
+	fmt.Printf("[simulation] finished %d matches in %s\n", count, duration)
+}
 
 func mainMenu(input *bufio.Reader) {
 	for {
@@ -120,11 +159,11 @@ func mainMenu(input *bufio.Reader) {
 
 		switch command {
 		case "1":
-			runSingleMatch()
+			runSingleMatch(true)
 		case "2":
 			runDoubleMatch()
 		case "3":
-			// runSimulation()
+			runSimulation(input)
 		case "no":
 			return
 		default:
