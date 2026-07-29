7af5ce8c2f2f7c37234d793c84d6b6de6aeaccf3
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-29 16:20:09 +0300

    feat: refactor url status checker to concurrent exec with logging middleware

--- assignments/05_status-checker/05_status-checker.go
+++ assignments/05_status-checker/05_status-checker.go
@@ -2,28 +2,52 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 )
 
 type URLResult struct {
-	URL        string
-	StatusCode int
+	URL        string `json:"url"`
+	StatusCode int    `json:"status_code"`
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			logTime := time.Since(start)
+			log.Printf("[%s] %s (duration: %s)", r.Method, r.URL, logTime)
+		})
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var body struct {
-		URLs []string
+		URLs []string `json:"urls"`
 	}
 
 	json.NewDecoder(r.Body).Decode(&body)
 
 	var results []URLResult
 
+	ch := make(chan URLResult)
+
 	for _, url := range body.URLs {
-		response, err := http.Get(url)
-		if err == nil {
-			results = append(results, URLResult{URL: url, StatusCode: response.StatusCode})
-		}
+		go func(u string) {
+			response, err := http.Get(u)
+			if err == nil {
+				ch <- URLResult{URL: u, StatusCode: response.StatusCode}
+				response.Body.Close()
+			} else {
+				ch <- URLResult{URL: u, StatusCode: 0}
+			}
+		}(url)
+	}
+
+	for i := 0; i < len(body.URLs); i++ {
+		res := <-ch
+		results = append(results, res)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -31,6 +55,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("POST /check", handler)
+	wrappedHandler := loggingMiddleware(http.HandlerFunc(handler))
+	http.Handle("POST /check", wrappedHandler)
 	http.ListenAndServe(":8080", nil)
 }
