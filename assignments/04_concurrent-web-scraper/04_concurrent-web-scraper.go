6e3780f2fd585e277ad5132eca64a26aaa234418
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-07-29 11:56:30 +0300

    feat: add error handling, implement worker pool with jobs and results channels - status code > 400 error handling - refactor: move .go file to directory for future use of go mod

--- assignments/04_concurrent-web-scraper/04_concurrent-web-scraper.go
+++ assignments/04_concurrent-web-scraper/04_concurrent-web-scraper.go
@@ -15,8 +15,6 @@ type Result struct {
 }
 
 func fetch(url string) Result {
-	fmt.Printf("[goroutine] fetching %s\n", url)
-
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
@@ -28,17 +26,35 @@ func fetch(url string) Result {
 	if err != nil {
 		return Result{
 			URL:      url,
-			Err:      err,
+			Err:      fmt.Errorf("-> timeout: %w", err),
 			Duration: duration,
 		}
 	}
 	defer response.Body.Close()
 
+	if response.StatusCode >= 400 {
+		return Result{
+			URL:        url,
+			StatusCode: response.StatusCode,
+			Err:        fmt.Errorf("-> http error %d: %s", response.StatusCode, http.StatusText(response.StatusCode)),
+			Duration:   duration,
+		}
+	}
+
 	return Result{
 		URL:        url,
 		StatusCode: response.StatusCode,
-		Err:        err,
+		Err:        nil,
 		Duration:   duration,
+	}
+}
+
+func worker(id int, jobs <-chan string, results chan<- Result, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for url := range jobs {
+		fmt.Printf("[worker-%d] is fetching %s\n", id, url)
+		results <- fetch(url)
 	}
 }
 
@@ -46,24 +62,28 @@ func main() {
 	urls := []string{
 		"https://www.google.com",
 		"https://www.facebook.com",
-		"https://www.youtube.com",
+		"https://www.youtuble.com",
+		"https://httpbin.org/status/404",
 		"https://www.reddit.com",
 	}
 
 	var wg sync.WaitGroup
 	results := make(chan Result, len(urls))
+	jobs := make(chan string, len(urls))
+
+	numWorkers := 2
+	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
+		go worker(w+1, jobs, results, &wg)
+	}
 
 	fmt.Printf("[main] fetching %d urls\n", len(urls))
+	start := time.Now()
 
 	for _, url := range urls {
-		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-
-			results <- fetch(url)
-		}()
+		jobs <- url
 	}
+	close(jobs)
 
 	go func() {
 		wg.Wait()
@@ -77,4 +97,7 @@ func main() {
 			fmt.Printf("[ok %d] fetched %s (%v)\n", res.StatusCode, res.URL, res.Duration)
 		}
 	}
+
+	fetchTime := time.Since(start)
+	fmt.Printf("[main] it took %s for %d workers to process %d urls\n", fetchTime, numWorkers, len(urls))
 }
