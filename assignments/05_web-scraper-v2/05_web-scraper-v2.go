b37a4e61f0d526f090a2e64bece015d34dd0aabf
Iustin Florian Gaiță <iustingaita.f@gmail.com>
2026-08-03 16:39:40 +0300

    feat: integrate MongoDB for session persistance - save session results to 'sessions' collection - automate MongoDB Docker container startup on app launch

--- assignments/05_web-scraper-v2/05_web-scraper-v2.go
+++ assignments/05_web-scraper-v2/05_web-scraper-v2.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"regexp"
 	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-type URLResult struct {
-	URL        string `json:"url"`
-	StatusCode int    `json:"status_code"`
-	Title      string `json:"title"`
-	Server     string `json:"server"`
+type CheckSession struct {
+	ID        bson.ObjectID `bson:"_id,omitempty" json:"id"`
+	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
+	TotalURLs int           `bson:"total_urls" json:"total_urls"`
+	Results   []URLResult   `bson:"results" json:"results"`
 }
+
+type URLResult struct {
+	URL        string `json:"url" bson:"url"`
+	StatusCode int    `json:"status_code" bson:"status_code"`
+	Title      string `json:"title,omitempty" bson:"title,omitempty"`
+	Server     string `json:"server,omitempty" bson:"server,omitempty"`
+}
+
+var sessionCollection *mongo.Collection
 
 func getTitle(html string) string {
 	re := regexp.MustCompile("(?i)<title>(.*?)</title>")
@@ -84,13 +98,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		results = append(results, res)
 	}
 
+	session := CheckSession{
+		CreatedAt: time.Now(),
+		TotalURLs: len(results),
+		Results: results,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	insertResult, err := sessionCollection.InsertOne(ctx, session)
+	if err != nil {
+		log.Fatalf("Error saving to MongoDB: %v", err)
+	} else {
+		log.Printf("Session successfully saved in MongoDB with ID: %v", insertResult.InsertedID)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(results)
 }
 
 func main() {
+	mongoURI := "mongodb://localhost:27017"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatalf("MongoDB connection error: %v", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("MongoDB doesn't respond: %v", err)
+	}
+	log.Println("MongoDB connection established")
+
+	sessionCollection = client.Database("web-scraper_db").Collection("sessions")
+
 	wrappedHandler := loggingMiddleware(http.HandlerFunc(handler))
 	http.Handle("POST /check", wrappedHandler)
+
 	log.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
